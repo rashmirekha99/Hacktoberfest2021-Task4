31da247ad355146d12799943ab9bfd6fd06dac02
rashmirekha99 <kothalawalarr.19@uom.lk>
2021-10-18 22:50:09 +0530

    Corrected Errors

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,29 +1,29 @@
-#include <studio.h>
-
+#include <stdio.h>
+#include<stdbool.h>
 
 int main()
 {
     bool swap;
-    int n=5,j;
+    int n=5,j,temp;
     int arr[5]={58,89,71,35,6};
     
     
     do{
         swap=false;
-        for(j=0;j<n;j++)
+        for(j=0;j<n-1;j++)
         {
-            if (arr[j]>arr[j-1]);
+            if (arr[j]>arr[j+1])
             {
                 temp=arr[j];
                 arr[j]=arr[j+1];
-                arr[j+1]=temp
-                swap=false;
+                arr[j+1]=temp;
+                swap=true;
             }
         }
     }while(swap);
     
-    for(m=0;m<n;m--)
+    for(int m=0;m<n;m++)
         {
-            printf("%s ",arr[l]);
+            printf("%d ",arr[m]);
         }
 }
